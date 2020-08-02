0f57c4cb820c5a3603ff030c33ce84c1a900d0f9
olivercastroteran <olivercastroteran@gmail.com>
2020-08-02 18:08:05 -0400

    loading images & injecting into html page

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -40,6 +41,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]',
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
